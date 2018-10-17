a124248ae6797a098f96fc0d1c1b6deec91228f8
Laureta Dzika <xhika_l@hotmail.com>
2018-10-17 11:04:33 +0200

    added pages
    *added index.php with basic html structure
    *added functions.php
    *added data.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * 
+ */ 
+require __DIR__.'/functions.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>fake news</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"> 
+</head>
+<body>
+
+</body>
+</html>
+
