d110da5c9ff05d05d5d0ec8b9e2367807d9e9f08
Laureta Dzika <xhika_l@hotmail.com>
2018-10-21 14:31:16 +0200

    added arrays

--- data.php
+++ data.php
@@ -1,2 +1,40 @@
 <?php
 declare(strict_types=1);
+
+$authors = [
+	[
+		'id' => 1,
+		'name' => 'John Snow',
+		'title' => 'Title example',
+		'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+		'likes' => 19
+	],
+	[
+		'id' => 2,
+		'name' => 'Arya Stark',
+		'title' => 'Title example',
+		'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+		'likes' => 27
+ 	],
+	[
+		'id' => 3,
+		'name' => 'Sansa Stark',
+		'title' => 'Title example',
+		'content' => 'Sansa\'s post',
+		'likes' => 10
+ 	],
+	[
+		'id' => 4,
+		'name' => 'Ned Stark',
+		'title' => 'Title example',
+		'content' => 'Ned\'s post',
+		'likes' => 31
+ 	],
+	[
+		'id' => 5,
+		'name' => 'Robb Stark',
+		'title' => 'Title example',
+		'content' => 'Robb\'s post',
+		'likes' => 22
+ 	],
+];
