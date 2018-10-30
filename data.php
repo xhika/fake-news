49e63f7d6e66d5486649ef9fe775b96edcb4ba2d
Laureta Dzika <xhika_l@hotmail.com>
2018-10-30 18:26:44 +0100

    gave authors multiple posts

--- data.php
+++ data.php
@@ -1,50 +1,118 @@
 <?php
 declare(strict_types=1);
 
-$authors = [
+$authors =
+[
 	[
-		'id' => 1,
 		'name' => 'Jon Snow',
-		'title' => 'First lesson: stick \'em with the pointy end.',
-		'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-		'likes' => 19,
-		'date' => 'Fri, 14 Sep 2018 21:20',
-		'img' => 'https://image.tmdb.org/t/p/original/2UrGpntyQtgunf039MErok78ZOK.jpg'
+		'posts' => 	[
+			[
+				'id' => 1,
+				'title' => 'First lesson: stick \'em with the pointy end.',
+				'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+				'date' => 'Fri, 14 Sep 2018 21:20',
+				'img' => 'https://image.tmdb.org/t/p/original/2UrGpntyQtgunf039MErok78ZOK.jpg',
+				'likes' => 11,
+			],
+			[
+				'id' => 2,
+				'title' => 'Second Post',
+				'content' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+				'date' => 'Mon, 1 Sep 2018 09:20',
+				'img' => 'https://image.tmdb.org/t/p/original/2UrGpntyQtgunf039MErok78ZOK.jpg',
+				'likes' => 22,
+			],
+		]
 	],
 	[
-		'id' => 2,
+
 		'name' => 'Arya Stark',
-		'title' => 'Has anyone seen my face?',
-		'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-		'likes' => 27,
-		'date' => 'Sat, 15 Sep 2018 06:55',
-		'img' => 'https://image.tmdb.org/t/p/original/x9wkYuEBVDP8IHJdgYL1frgO9OJ.jpg'
- 	],
+		'posts' => [
+			[
+				'id' => 3,
+				'title' => 'Has anyone seen my face?',
+				'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum..',
+				'date' => 'Mon, 29 Oct 2018 06:55',
+				'img' => 'https://image.tmdb.org/t/p/original/x9wkYuEBVDP8IHJdgYL1frgO9OJ.jpg',
+				'likes' => 27,
+			],
+			[
+				'id' => 4,
+				'title' => 'Arya Second Post',
+				'content' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+				'date' => 'Sat, 15 Sep 2018 06:55',
+				'img' => 'https://image.tmdb.org/t/p/original/x9wkYuEBVDP8IHJdgYL1frgO9OJ.jpg',
+				'likes' => 22
+			],
+		],
+	],
 	[
-		'id' => 3,
+
 		'name' => 'Tyrion Lannister',
-		'title' => 'Fetch me more wine!',
-		'content' => 'Tyrion needs more wine.',
-		'likes' => 50,
-		'date' => 'Mon, 8 Oct 2018 18:30',
-		'img' => 'https://image.tmdb.org/t/p/original/aKz3lXU71wqdslC1IYRC3yHD6yw.jpg'
- 	],
+		'posts' => [
+			[
+				'id' => 5,
+				'title' => 'Fetch me wine!',
+				'content' => 'Tyrion needs wine.',
+				'date' => 'Mon, 29 Oct 2018 06:55',
+				'img' => 'https://image.tmdb.org/t/p/original/aKz3lXU71wqdslC1IYRC3yHD6yw.jpg',
+				'likes' => 27,
+			],
+			[
+				'id' => 6,
+				'title' => 'Tyrion needs another round of wine.',
+				'content' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+				'date' => 'Sat, 15 Sep 2018 06:55',
+				'img' => 'https://image.tmdb.org/t/p/original/aKz3lXU71wqdslC1IYRC3yHD6yw.jpg',
+				'likes' => 22
+			],
+		]
+	],
 	[
-		'id' => 4,
+
 		'name' => 'Ned Stark',
-		'title' => 'I heard winter is coming.',
-		'content' => 'Ned\'s post. Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-		'likes' => 31,
-		'date' => 'Sun, 28 Oct 2018 13:34',
-		'img' => 'https://image.tmdb.org/t/p/original/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg'
- 	],
+		'posts' => [
+			[
+				'id' => 7,
+				'title' => 'I heard winter is coming.',
+				'content' => 'Ned\'s post. Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+				'likes' => 31,
+				'date' => 'Sun, 28 Oct 2018 13:34',
+				'img' => 'https://image.tmdb.org/t/p/original/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg'
+			],
+			[
+				'id' => 8,
+				'title' => 'Second I heard winter is coming.',
+				'content' => 'Ned\'s post. Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+				'likes' => 31,
+				'date' => 'Sun, 28 Oct 2018 13:34',
+				'img' => 'https://image.tmdb.org/t/p/original/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg'
+			],
+		]
+	],
 	[
-		'id' => 5,
+
 		'name' => 'Daenerys Targaryen',
-		'title' => 'Dracarys',
-		'content' => 'Khaleesi\'s post. Drogons latest Dracarys.',
-		'likes' => 999,
-		'date' => 'Sat, 8 Sep 2018 16:01',
-		'img' => 'https://i.ytimg.com/vi/QzHix_cuGZ8/hqdefault.jpg'
- 	],
+		'posts' => 	[
+			[
+				'id' => 9,
+				'title' => 'Dracarys',
+				'content' => 'Khaleesi\'s post. Drogons latest Dracarys.',
+				'likes' => 999,
+				'date' => 'Sat, 8 Sep 2018 16:01',
+				'img' => 'https://i.ytimg.com/vi/QzHix_cuGZ8/hqdefault.jpg'
+			],
+			[
+				'id' => 10,
+				'title' => '2 Dracarys',
+				'content' => 'Khaleesi\'s post. Drogons latest Dracarys.',
+				'likes' => 999,
+				'date' => 'Tue, 30 Oct 2018 09:01',
+				'img' => 'https://i.ytimg.com/vi/QzHix_cuGZ8/hqdefault.jpg'
+			],
+		]
+	],
 ];
+
+
+
