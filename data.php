d22b42da54e6a9f867ed45efa3df2dc1ce590915
Laureta Dzika <xhika_l@hotmail.com>
2018-10-31 14:08:29 +0100

    updated array with new pics

--- data.php
+++ data.php
@@ -19,7 +19,7 @@ $authors =
 				'title' => 'Second Post',
 				'content' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
 				'date' => 'Mon, 1 Sep 2018 09:20',
-				'img' => 'https://image.tmdb.org/t/p/original/2UrGpntyQtgunf039MErok78ZOK.jpg',
+				'img' => 'https://img.huffingtonpost.com/asset/56e024e81500002a000b1742.jpeg?cache=jzkjufoi3w&ops=scalefit_720_noupscale',
 				'likes' => 22,
 			],
 		]
@@ -32,7 +32,7 @@ $authors =
 				'id' => 3,
 				'title' => 'Has anyone seen my face?',
 				'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum..',
-				'date' => 'Mon, 29 Oct 2018 06:55',
+				'date' => 'Mon, 29 Oct 2018 08:55',
 				'img' => 'https://image.tmdb.org/t/p/original/x9wkYuEBVDP8IHJdgYL1frgO9OJ.jpg',
 				'likes' => 27,
 			],
@@ -40,8 +40,8 @@ $authors =
 				'id' => 4,
 				'title' => 'Arya Second Post',
 				'content' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-				'date' => 'Sat, 15 Sep 2018 06:55',
-				'img' => 'https://image.tmdb.org/t/p/original/x9wkYuEBVDP8IHJdgYL1frgO9OJ.jpg',
+				'date' => 'Sat, 15 Sep 2018 09:28',
+				'img' => 'https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2017/08/23/Pictures/_9175785c-87ea-11e7-817c-4caf18ee223c.jpeg',
 				'likes' => 22
 			],
 		],
@@ -54,7 +54,7 @@ $authors =
 				'id' => 5,
 				'title' => 'Fetch me wine!',
 				'content' => 'Tyrion needs wine.',
-				'date' => 'Mon, 29 Oct 2018 06:55',
+				'date' => 'Mon, 29 Oct 2018 22:55',
 				'img' => 'https://image.tmdb.org/t/p/original/aKz3lXU71wqdslC1IYRC3yHD6yw.jpg',
 				'likes' => 27,
 			],
@@ -62,8 +62,8 @@ $authors =
 				'id' => 6,
 				'title' => 'Tyrion needs another round of wine.',
 				'content' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-				'date' => 'Sat, 15 Sep 2018 06:55',
-				'img' => 'https://image.tmdb.org/t/p/original/aKz3lXU71wqdslC1IYRC3yHD6yw.jpg',
+				'date' => 'Sat, 15 Sep 2018 04:55',
+				'img' => 'https://nerdist.com/wp-content/uploads/2017/08/GoT-Tyrion-Featured-08282017.jpg',
 				'likes' => 22
 			],
 		]
@@ -85,8 +85,8 @@ $authors =
 				'title' => 'Second I heard winter is coming.',
 				'content' => 'Ned\'s post. Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
 				'likes' => 31,
-				'date' => 'Sun, 28 Oct 2018 13:34',
-				'img' => 'https://image.tmdb.org/t/p/original/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg'
+				'date' => 'Sun, 28 Oct 2018 23:34',
+				'img' => 'https://images.indianexpress.com/2017/05/ned-stark-759.jpg'
 			],
 		]
 	],
@@ -99,8 +99,8 @@ $authors =
 				'title' => 'Dracarys',
 				'content' => 'Khaleesi\'s post. Drogons latest Dracarys.',
 				'likes' => 999,
-				'date' => 'Sat, 8 Sep 2018 16:01',
-				'img' => 'https://i.ytimg.com/vi/QzHix_cuGZ8/hqdefault.jpg'
+				'date' => 'Sat, 8 Sep 2018 13:37',
+				'img' => 'http://photos.vanityfair.com/2014/04/21/53546e15245ef3601913b264_gameofthrones14_64.jpg'
 			],
 			[
 				'id' => 10,
@@ -108,7 +108,7 @@ $authors =
 				'content' => 'Khaleesi\'s post. Drogons latest Dracarys.',
 				'likes' => 999,
 				'date' => 'Tue, 30 Oct 2018 09:01',
-				'img' => 'https://i.ytimg.com/vi/QzHix_cuGZ8/hqdefault.jpg'
+				'img' => 'https://memegenerator.net/img/instances/48953665.jpg'
 			],
 		]
 	],
